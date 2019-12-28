944cda5c0d0657479e0f2e74f4b54d0399700b2a
bugnofree <pwnkeeper@gmail.com>
2019-12-28 18:12:39 +0800

    Banner image swiper

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,12 +8,32 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  List _imgurls = [
+    'http://img.zcool.cn/community/01fa5b590f1bb5a80121455078583b.jpg@1280w_1l_2o_100sh.jpg',
+    'http://photo.tuchong.com/27323/f/397935.jpg',
+    'http://bpic.588ku.com/back_pic/03/70/88/6957b6eb81a4858.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
-      )
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 5,
+              child: Swiper(
+                itemCount: _imgurls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(_imgurls[index], fit: BoxFit.fill);
+                },
+                pagination: SwiperPagination(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
